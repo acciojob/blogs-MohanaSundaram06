daf4d0f4a302d72bf96a2e8fb0cd57b2b68d8d20
Mohana Sundaram K <mohanasundaramk06@gmail.com>
2023-06-16 01:34:51 +0530

    Date modified

--- src/main/java/com/driver/models/Blog.java
+++ src/main/java/com/driver/models/Blog.java
@@ -1,5 +1,7 @@
 package com.driver.models;
 
+import org.springframework.data.annotation.CreatedDate;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
@@ -17,6 +19,7 @@ public class Blog{
 
     private String content;
 
+    @CreatedDate
     private Date pubDate;
 
     @ManyToOne
