d497cf1217807bceaa0597080030d665689b0850
Mohana Sundaram K <mohanasundaramk06@gmail.com>
2023-06-16 17:27:35 +0530

    test1

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -28,6 +28,7 @@ public class BlogService {
         Blog blog = new Blog();
         blog.setTitle(title);
         blog.setContent(content);
+        blog.setPubDate(new Date());
         blog.setUser(user);
 
         user.getBlogList().add(blog);
